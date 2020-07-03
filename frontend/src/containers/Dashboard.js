ab3dac1b138a92e460c7190dad6241c92398a4aa
Faizar Ali <alifaizar@gmail.com>
2020-07-03 16:36:33 -0400

    Dashboard

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./Dashboard.css";
+import { Tabs, Tab } from 'react-bootstrap-tabs';
 
 export default function Dashboard() {
-    
-    
-    return (
-        <div class="topnav">
-        <a href="#home">CoronaCards</a>
-        <a href="#collection">Collection</a>
-        <a href="#statistics">Statistics</a>
-        <a href="#account">Account</a>
-        <a href="#about">About</a>
 
-      </div>
-    );
-  }
+
+  return (
+    <div class="topnav">
+      <Tabs onSelect={(index, label) => console.log(label + ' selected')}>
+        <Tab class="Tab1" label="CoronaCards">Tab 1 content</Tab>
+        <Tab label="Collection">Tab 2 content</Tab>
+        <Tab label="Statistics">Tab 3 content</Tab>
+        <Tab label="Account">Tab 4 content</Tab>
+        <Tab label="About">Tab 5 content</Tab>
+      </Tabs>
+
+    </div>
+
+  );
+}
