cb0180e506a48e8786c15d0417dfa6177e480fbf
Faizar Ali <54734200+faizarali@users.noreply.github.com>
2020-07-03 17:34:04 +0300

    Add files via upload

---
+++ frontend/src/containers/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./Login.css";
+
+export default function Login() {
+  //const [email, setEmail] = useState("");
+  const [UserName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return UserName.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="UserName" bsSize="large">
+          <FormLabel>Username</FormLabel>
+          <FormControl
+            autoFocus
+            type="UserName"
+            value={UserName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
---
+++ frontend/src/containers/Register.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./Register.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //   const [FirstName, setFirstName] = useState("");
+  //   const [LastName, setLastName] = useState("");
+  const [UserName, setUserName] = useState("");
+
+  function validateForm() {
+    return UserName.length > 0 && email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Register">
+      <form onSubmit={handleSubmit}>
+        {/* <FormGroup controlId="FirstName" bsSize="large">
+          <FormLabel>First Name</FormLabel>
+          <FormControl
+            autoFocus
+            type="FirstName"
+            value={FirstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </FormGroup> */}
+        <FormGroup controlId="UserName" bsSize="large">
+          <FormLabel>Username</FormLabel>
+          <FormControl
+            autoFocus
+            type="UserName"
+            value={UserName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="email" bsSize="large">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Register
+        </Button>
+      </form>
+    </div>
+  );
+}
