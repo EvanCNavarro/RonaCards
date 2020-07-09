ad67ae44bf20ee517e3a3d630654325903d4dd14
muditjains <muditjains@yahoo.com>
2020-07-08 22:27:02 -0400

    Worked on the account page

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -5,15 +5,28 @@ import { Tabs, Tab } from 'react-bootstrap-tabs';
 
 export default function Dashboard() {
 
+const vari = 10;
+const username = "Mudit";
+const email = "faizarali@corona.com";
 
   return (
     <div class="topnav">
-      <Tabs onSelect={(index, label) => console.log(label + ' selected')}>
-        <Tab label="CoronaCards">Tab 1 content</Tab>
-        <Tab label="Collection">Tab 2 content</Tab>
-        <Tab label="Statistics">Tab 3 content</Tab>
-        <Tab label="Account">Tab 4 content</Tab>
-        <Tab label="About">Tab 5 content</Tab>
+      <Tabs  class = "topbar" onSelect={(index, label) => console.log(label + ' selected')}>
+        <Tab  label="CoronaCards">TAB  1 CONTENT</Tab>
+        <Tab  label="Collection">Tab 2 content</Tab>
+        <Tab  label="Statistics">Tab 3 content</Tab>
+        <Tab  label="Account">
+          <h2>Username: {username}</h2>
+          <h2>Email: {email}</h2>
+          <h2>Password: ******  <button class="password_reset_button">Reset Password</button></h2>
+          <h2>Card Count: {vari}</h2>
+
+
+
+          </Tab>
+        <Tab  label="About">
+       </Tab>
+        <Tab  label="Logout"> </Tab>
       </Tabs>
 
     </div>
