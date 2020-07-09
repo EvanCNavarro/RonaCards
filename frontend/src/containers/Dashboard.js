b3f32c08719de3dd06aff64ff0d497c14d6ba043
muditjains <muditjains@yahoo.com>
2020-07-09 17:08:11 -0400

    Made things better for the account's section

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import "./Dashboard.css";
 import { Tabs, Tab } from 'react-bootstrap-tabs';
+import contact_logo from './account_icon.png'; 
+
 
 export default function Dashboard() {
 
-  const vari = 10; // connect to API
+  const card_counter = 10; // connect to API
   const username = "Mudit"; // connect to API
   const email = "faizarali@corona.com"; // connect to API
 
@@ -14,20 +16,23 @@ export default function Dashboard() {
       <Tabs onSelect={(index, label) => console.log(label + ' selected')}>
         <Tab class="topbar" label="CoronaCards">TAB  1 CONTENT</Tab>
         <Tab class="topbar" label="Collection">Tab 2 content</Tab>
+
         <Tab class="topbar" label="Statistics">Tab 3 content</Tab>
-        <Tab class="topbar" label="Account">
+        <Tab  label="Account">
+
           <div class="account_box_div">
-            {/* <img src="account_icon.png" width="500" height="600">
-            </img>  */}
+            <img class ="account_icon" src={contact_logo} alt = "Contact Logo"/>
+            <div class = "account_details_box">
             <p><b>Username:</b> {username}</p>
             <p><b>Email:</b> {email}</p>
             <p><b>Password:</b> ******** <button class="password_reset_button">Reset Password</button></p>
-            <p><b>Card Count:</b> {vari}</p>
+            <p><b>Card Count:</b> {card_counter}</p>
+            <button class="logout_button">Logout</button>
+            </div>
+           
           </div>
-
-
-
         </Tab>
+        
         <Tab label="About">
           <h2 class="overview">OVERVIEW</h2>
           <br />
@@ -39,7 +44,6 @@ export default function Dashboard() {
           <br />
           <h2 class="surprise">Collect all of the cards for a surprise!</h2>
         </Tab>
-        <Tab label="Logout"> </Tab>
       </Tabs>
 
     </div >
