b782c5ea61cb10e24855c722e9a058f1b25f29c2
Faizar Ali <alifaizar@gmail.com>
2020-07-18 18:11:36 -0400

    Working on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import ReactReact, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dashboard from "./containers/Dashboard";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
 import ForgotPassword from "./containers/ForgotPassword";
+import UserContext from "./context/UserContext";
 
 import MainPage from './containers/MainPage';
 
 export default function App() {
-  return ( 
-    <>
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-      {/* <Route  path="/dashboard" component={Dashboard} /> */}
-      <Route  path="/login" component={Login} />
-      <Route  path="/register" component={Register} />
-      <Route  path="/passwordReset" component={ForgotPassword} />
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
 
-    </Switch>
-  </BrowserRouter>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+        <UserContext.Provider value={userData, setUserData}>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            {/* <Route  path="/dashboard" component={Dashboard} /> */}
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/passwordReset" component={ForgotPassword} />
+
+          </Switch>
+        </UserContext.Provider>
+      </BrowserRouter>
+    </>
   )
-  
+
 }
 
 
