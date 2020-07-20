cbf18b392c11b780dd1d9b0bed65e70bef2e398e
Faizar Ali <alifaizar@gmail.com>
2020-07-20 13:52:33 -0400

    Started Unit Tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('My Test Case', () => {
+    expect(true).toEqual(true);
+});
