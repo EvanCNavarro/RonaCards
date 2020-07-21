9cc5ebce84477920bfb41c93f81aaddec5ca2125
Faizar Ali <alifaizar@gmail.com>
2020-07-21 19:32:15 -0400

    Test_3

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('My First Test Case', () => {
+    expect(true).toEqual(true);
+});
+
+it('Testing if Div Works', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
-it('My Test Case', () => {
-    expect(true).toEqual(true);
-});
-
-it('renders without crashing_2', () => {
+it('Testing if Switch Headers Work', () => {
     const Switch = document.createElement('Switch');
     ReactDOM.render(<App />, Switch);
     ReactDOM.unmountComponentAtNode(Switch);
+});
+
+it('Testing if Routing Works', () => {
+    const Route = document.createElement('Route');
+    ReactDOM.render(<App />, Route);
+    ReactDOM.unmountComponentAtNode(Route);
 });
