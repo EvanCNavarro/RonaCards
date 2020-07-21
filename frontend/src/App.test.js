0d3ffb6dbafcd23ffc59dad9e83a6f1d0c3a29cc
Faizar Ali <alifaizar@gmail.com>
2020-07-21 19:43:31 -0400

    Test_4

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const { MongoDB } = require('mongodb');
-require('custom-env').env('dev');
+// const { MongoDB } = require('mongodb');
+// require('custom-env').env('dev');
 
 it('My First Test Case', () => {
     expect(true).toEqual(true);
