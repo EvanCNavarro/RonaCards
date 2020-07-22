79259d10ffd2227ea5edd873c2e4251a1e4b2094
evancnavarro <evancnavarro@gmail.com>
2020-07-22 14:43:27 +0000

    website styling

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthOptions from '../auth/AuthOptions'
+import viruses from '../../images/viruses.png';
 
 export default function Header() {
 	return (
 		<header id ="header">
 			<Link to="/">
-				<h1 className = "title">Rona Cards</h1>
+				<div className="homebase">
+					<img src={ viruses } alt="Viruses" height="23px" />
+					<h1 className = "title">Rona Cards</h1>
+				</div>
 			</Link>
 			<AuthOptions />
 		</header>
