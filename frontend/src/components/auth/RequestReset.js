a316e38f0adaa40b59bc81333885638ee8605288
Faizar Ali <alifaizar@gmail.com>
2020-07-21 21:55:57 -0400

    Test_8

--- frontend/src/components/auth/RequestReset.js
+++ frontend/src/components/auth/RequestReset.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
-import UserContext from "../../context/UserContext";
+// import UserContext from "../../context/UserContext";
 import Axios from "axios";
 import ErrorNotice from "../misc/ErrorNotice";
 
@@ -8,7 +8,7 @@ export default function RequestReset() {
 	const [email, setEmail] = useState();
 	const [error, setError] = useState();
 
-	const { setUserData } = useContext(UserContext);
+	// const { setUserData } = useContext(UserContext);
 	const history = useHistory();
 
 	const submit = async (e) => {
@@ -21,27 +21,27 @@ export default function RequestReset() {
 				userEmail
 			);
 			history.push("/sent");
-		// create check api, gives back user id, after id, send email
+			// create check api, gives back user id, after id, send email
 		} catch (err) {
 			err.response.data.msg && setError(err.response.data.msg);
 		}
 	};
 
-	return ( <div className = "page">
-			<h2>Get Link to Reset Password:</h2>
-			{error && (
-				<ErrorNotice message={error} clearError={() => setError(undefined)} />
-			)}
-			<form className="form" onSubmit = { submit }>
-				<label htmlFor="login-email">Email: </label>
-				<input
-					id="login-email"
-					type="email"
-					onChange={(e) => setEmail(e.target.value)}
-				/>
+	return (<div className="page">
+		<h2>Get Link to Reset Password:</h2>
+		{error && (
+			<ErrorNotice message={error} clearError={() => setError(undefined)} />
+		)}
+		<form className="form" onSubmit={submit}>
+			<label htmlFor="login-email">Email: </label>
+			<input
+				id="login-email"
+				type="email"
+				onChange={(e) => setEmail(e.target.value)}
+			/>
 
-				<input type="submit" value="Send Reset Link" />
-			</form>
-		</div>
+			<input type="submit" value="Send Reset Link" />
+		</form>
+	</div>
 	);
 }
