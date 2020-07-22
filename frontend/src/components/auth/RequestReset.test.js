7808f7a4a52a8ee8571e26f829242a5b5a96e059
Faizar Ali <alifaizar@gmail.com>
2020-07-21 21:53:57 -0400

    Test_8

--- frontend/src/components/auth/RequestReset.test.js
+++ frontend/src/components/auth/RequestReset.test.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Collection from './Collection';
+import RequestReset from './RequestReset';
 
 it('Testing if Div Works', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Collection />, div);
+    ReactDOM.render(<RequestReset />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
 it('Testing if Switch Work', () => {
     const Switch = document.createElement('Switch');
-    ReactDOM.render(<Collection />, Switch);
+    ReactDOM.render(<RequestReset />, Switch);
     ReactDOM.unmountComponentAtNode(Switch);
 });
 
 it('Testing if BrowserRouter Works', () => {
     const BrowserRouter = document.createElement('BrowserRouter');
-    ReactDOM.render(<Collection />, BrowserRouter);
+    ReactDOM.render(<RequestReset />, BrowserRouter);
     ReactDOM.unmountComponentAtNode(BrowserRouter);
 });
 
 it('Testing if Routing Works', () => {
     const Route = document.createElement('Route');
-    ReactDOM.render(<Collection />, Route);
+    ReactDOM.render(<RequestReset />, Route);
     ReactDOM.unmountComponentAtNode(Route);
 });
