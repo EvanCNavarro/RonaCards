892e958af31605d1fe31a4d9d37af312c31357b5
Faizar Ali <alifaizar@gmail.com>
2020-07-21 23:26:19 -0400

    Test_9

--- frontend/src/components/layout/Header.test.js
+++ frontend/src/components/layout/Header.test.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RequestReset from './RequestReset';
+import Header from './Header';
 
 it('Testing if Div Works', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<RequestReset />, div);
+    ReactDOM.render(<Header />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
 it('Testing if Switch Work', () => {
     const Switch = document.createElement('Switch');
-    ReactDOM.render(<RequestReset />, Switch);
+    ReactDOM.render(<Header />, Switch);
     ReactDOM.unmountComponentAtNode(Switch);
 });
 
 it('Testing if BrowserRouter Works', () => {
     const BrowserRouter = document.createElement('BrowserRouter');
-    ReactDOM.render(<RequestReset />, BrowserRouter);
+    ReactDOM.render(<Header />, BrowserRouter);
     ReactDOM.unmountComponentAtNode(BrowserRouter);
 });
 
 it('Testing if Routing Works', () => {
     const Route = document.createElement('Route');
-    ReactDOM.render(<RequestReset />, Route);
+    ReactDOM.render(<Header />, Route);
     ReactDOM.unmountComponentAtNode(Route);
 });
