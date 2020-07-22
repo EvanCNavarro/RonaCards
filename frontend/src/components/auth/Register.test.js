5a1e458ed377427a409187bd66104f1d263d1870
Faizar Ali <alifaizar@gmail.com>
2020-07-21 21:43:55 -0400

    Test_6

---
+++ frontend/src/components/auth/Register.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Register from './Register';
+
+it('Testing if Div Works', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Register />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Testing if Switch Work', () => {
+    const Switch = document.createElement('Switch');
+    ReactDOM.render(<Register />, Switch);
+    ReactDOM.unmountComponentAtNode(Switch);
+});
+
+it('Testing if BrowserRouter Works', () => {
+    const BrowserRouter = document.createElement('BrowserRouter');
+    ReactDOM.render(<Register />, BrowserRouter);
+    ReactDOM.unmountComponentAtNode(BrowserRouter);
+});
+
+it('Testing if Routing Works', () => {
+    const Route = document.createElement('Route');
+    ReactDOM.render(<Register />, Route);
+    ReactDOM.unmountComponentAtNode(Route);
+});
