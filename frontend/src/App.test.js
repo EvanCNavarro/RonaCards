833a250703feeab8f2a14850f629b473e68244d1
Faizar Ali <alifaizar@gmail.com>
2020-07-22 17:37:58 -0400

    Test_14

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -86,6 +86,6 @@ const users = [
     'Faizar',
     'Evan'
 ];
-test('test for a value in users', function () {
+test('Test for a value in users', function () {
     expect(users).toEqual(expect.arrayContaining(['Mudit', 'Evan']));
 });
